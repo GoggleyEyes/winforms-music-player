4c5ed4d0abe20f85ffad9cb91c3afb513c87dfdc
Shahbaz Hamid <shahbaz_hamid@hotmail.com>
2019-01-27 11:45:34 +0000

    Added test comment

--- MusicPlayer/Program.cs
+++ MusicPlayer/Program.cs
@@ -8,6 +8,7 @@ namespace MusicPlayer
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// Added a test comment
         [STAThread]
         static void Main()
         {
