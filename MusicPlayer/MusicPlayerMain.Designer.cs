c21201e08db570f36fdf45637acb8da7a3ab8c91
Shahbaz Hamid <shahbaz_hamid@hotmail.com>
2019-02-06 13:32:27 +0000

    Changed label colour

--- MusicPlayer/MusicPlayerMain.Designer.cs
+++ MusicPlayer/MusicPlayerMain.Designer.cs
@@ -311,7 +311,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(444, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 13);
